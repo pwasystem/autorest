16ea3c1c38ad1e733cf02fe82b3d77257694dc89
pwasystem <webmaster@pwasystem.com>
2024-01-15 11:03:11 -0300

    version 1

--- autorest.js
+++ autorest.js
@@ -14,6 +14,7 @@ class Autorest {
 			password : conf.database.pass,
 			database : conf.database.schema,
 			connectionLimit: 5
+			//criar comando para tratar resuldados incompatíveis
 		});
 		this.rest = {};
 	}
